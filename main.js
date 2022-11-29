57eb5b6995edb7237d83dea539259f06f10bbee2
1234Suhani <95090057+1234Suhani@users.noreply.github.com>
2022-11-29 16:55:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(600,460);
+    canvas.center();
+
+}
+
+function snapshot(){
+    save("St. Fairy Annual");
+}
+
+function draw(){
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
